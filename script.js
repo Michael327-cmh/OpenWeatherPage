c4b1ca717ccd70f2ae0514909a3ee743804a328b
Michael Carmody <mr.carmody89@gmail.com>
2021-07-19 15:00:18 -0400

    Updating JS & HTML

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
-var ATLsearch = document.querySelector("ATLPopulous");
+var repoList = document.querySelector('p');
+var fetchButton = document.getElementById('ATLPopulous');
 
+function getApi() {
+  // replace `octocat` with anyone else's GitHub username
+  var requestUrl = 'https://api.github.com/users/octocat/repos';
+
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      for (var i = 0; i < data.length; i++) {
+        var listItem = document.createElement('li');
+        listItem.textContent = data[i].html_url;
+        repoList.appendChild(listItem);
+      }
+    });
+}
+
+fetchButton.addEventListener('click', getApi);
